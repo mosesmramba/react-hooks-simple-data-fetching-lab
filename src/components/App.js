c9ba92a64d06ae43a3c78c4a925b0a8a4a601be1
mosesmramba <moses.mramba@student.moringaschool.com>
2023-10-27 09:00:31 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPicture, setDogPic] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPic(data.message);
+      });
+  }, []);
+  
+  if (!dogPicture) return <p>Loading...</p>;
+
+  return <img src={dogPicture} alt="A Random Dog" />;
+}
+
+export default App;
